154dc933024848dfc4bac72c7ea79c3794574546
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-07-24 22:20:28 -0400

    add habit form

--- src/components/HabitsForm/HabitsForm.jsx
+++ src/components/HabitsForm/HabitsForm.jsx
@@ -1,10 +1,70 @@
 import { useState } from "react";
 import * as habitsAPI from "../../utilities/habits-api";
+import "./HabitsForm.css";
 
 export default function HabitsForm() {
+  const [newHabit, setNewHabit] = useState({});
+  const [error, setError] = useState("");
+
+  function handleChange(event) {
+    setNewHabit({
+      ...newHabit,
+      [event.target.name]: event.target.value,
+    });
+    setError("");
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      await habitsAPI.createHabit({ ...newHabit });
+
+      setNewHabit({});
+    } catch {
+      setError("Invalid Habit - Try Again");
+    }
+  }
+
   return (
     <div>
       <h3>create a habit</h3>
+      <form className="create-habit" onSumbit={handleSubmit}>
+        <label>Habit Name:</label>
+        <input
+          type="text"
+          onChange={handleChange}
+          value={newHabit.habitName}
+          name="habitName"
+          required
+        />
+
+        <label>Start Date</label>
+        <input
+          type="date"
+          onChange={handleChange}
+          value={newHabit.startDate}
+          name="startDate"
+          required
+        />
+
+        <label>End Date</label>
+        <input
+          type="date"
+          onChange={handleChange}
+          value={newHabit.endDate}
+          name="endDate"
+        />
+
+        <label>Duration(hours)</label>
+        <input
+          type="number"
+          onChange={handleChange}
+          value={newHabit.duration}
+          name="duration"
+          required
+        />
+        <button>Add Habit</button>
+      </form>
     </div>
   );
 }
